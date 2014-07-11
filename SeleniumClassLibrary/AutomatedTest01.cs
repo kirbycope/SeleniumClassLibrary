ee5a96b107499f4a73b7bac2f41290c9e066eb58
kirbycope@gmail.com <timothy1>
2014-07-11 15:47:34 -0600

    Initial Checkin
    Checking in the C# Visual Studio, Selenium WebDriver, and NUnit project.

---
+++ SeleniumClassLibrary/AutomatedTest01.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using NUnit.Framework;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumClassLibrary
+{
+    [TestFixture]
+    public class AutomatedTest01
+    {
+
+        IWebDriver driver;
+
+        [TestFixtureSetUp] // http://www.nunit.org/index.php?p=testFixture&r=2.5
+        public void TestSetUp()
+        {
+            // Set the IWebDriver to use FireFox
+            driver = new FirefoxDriver();
+
+            // Set the timeout to 30 seconds
+            driver.Manage().Timeouts().ImplicitlyWait(new TimeSpan(0, 0, 30));
+
+        }
+
+        [Test] // http://www.nunit.org/index.php?p=test&r=2.5
+        public void NavigateToGoogle()
+        {
+            // Navigate to URL
+            driver.Navigate().GoToUrl("http://www.google.com");
+
+            // Assert that the IWebDriver window's title is "Google"
+            Assert.AreEqual("Google", driver.Title);
+        }
+
+        [TestFixtureTearDown] // http://www.nunit.org/index.php?p=fixtureTeardown&r=2.5
+        public void FixtureTearDown()
+        {
+            // Closes all windows associated with the IWebDriver
+            driver.Quit();
+        }
+    }
+}
---
+++ SeleniumClassLibrary/AutomatedTest02.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumClassLibrary
+{
+    // Genereated by Selenuim IDE for Firefox
+    [TestFixture]
+    public class AutomatedTest02
+    {
+
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "http://timothycope.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void The01Test()
+        {
+            driver.Navigate().GoToUrl(baseURL + "/");
+            Thread.Sleep(2500);
+            driver.FindElement(By.LinkText("ABOUT TIMOTHY COPE")).Click();
+            Thread.Sleep(2500);
+            for (int second = 0; ; second++)
+            {
+                if (second >= 60) Assert.Fail("timeout");
+                try
+                {
+                    if (IsElementPresent(By.XPath("//img[@alt='logo']"))) break;
+                }
+                catch (Exception)
+                { }
+                Thread.Sleep(1000);
+            }
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
